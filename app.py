e616d17a2bfb349bd85f514a5654dc42fb69845e
Shwetha Sundararaman <shwethas@microsoft.com>
2025-04-02 12:27:07 +0800

    Changes 2

--- app.py
+++ app.py
@@ -1,23 +1,35 @@
 from azure.ai.inference import ChatCompletionsClient
-from azure.core.credentials import AzureKeyCredential
 from azure.ai.inference.models import SystemMessage, UserMessage
+from azure.core.credentials import AzureKeyCredential
 import streamlit as st
 
 endpoint = st.secrets["endpoint"]
-token = st.secrets["token"]
 model_name = st.secrets["model_name"]
+token = st.secrets["token"]
 
 client = ChatCompletionsClient(
     endpoint=endpoint,
     credential=AzureKeyCredential(token),
 )
 
-response = client.complete(
-    messages=[
-        UserMessage(content="How many languages are in the world?"),
-    ],
-    max_tokens=1000,
-    model=model_name
-)
+# response = client.complete(
+#     messages=[
+#         SystemMessage(content="You are a helpful assistant."),
+#         UserMessage(content="I am going to Paris, what should I see?")
+#     ],
+#     max_tokens=1000,
+#     model=model_name
+# )
 
-print(response.choices[0].message.content)
+# print(response.choices[0].message.content)
+
+def get_response(prompt):
+    response = client.complete(
+        stream=True,
+        messages=[
+            UserMessage(content=prompt)
+        ],
+        max_tokens=1000,
+        model=model_name
+    )
+    return response
